59bf864fc4302e32cc4f04fca04530f916ad2420
karnakar <karnakar@ymail.com>
2020-02-05 21:57:14 -0500

    first commit

---
+++ src/test/java/kt/reposearch/KtReposearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kt.reposearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtReposearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
